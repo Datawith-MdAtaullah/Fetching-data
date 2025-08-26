041a86fa42ddc181e4e1c2739bfb086d4a4dd712
Datawith-MdAtaullah <info.mdataullah@gmail.com>
2025-08-26 16:02:36 +0530

    Initial commit

---
+++ fetch.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+response = requests.get("https://php.enigmagenomics.com/api/gene.php?search=AAAS")
+print(response.status_code)  # getting status code = 200 means everything is OK 
+
+API = "https://php.enigmagenomics.com/api/gene.php?search="
+genes = ["AAAS","ATM","BRCA1"]
+results = []
+
+for i in genes:
+    url = API + i
+    print("Fetching data from:", url)
+    
+    x = requests.get(url,timeout=20)
+    
+    if x.status_code != 200:
+        print('Error')
+        continue
+    data = x.json()
+    gene_name = data.get("name",i)
+    
+    conditions=[]
+    for j in data.get("details",{}).get("conditions",[]):
+        y = j.get("name")
+        if y:
+            conditions.append(y)
+    results.append({
+        "gene": gene_name,
+        "conditions" : conditions
+    })        
+    
+
+output_file = "gene_condition_datax.json"
+with open (output_file,"w",encoding="utf-8") as f:
+    json.dump(results,f,indent = 2)
+    
